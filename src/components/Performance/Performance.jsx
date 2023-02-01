d37ee9a203e1481f153aea5da3b4be1abb2e7947
julien382 <julhermain@gmail.com>
2023-02-01 15:36:24 +0100

    performance

--- src/components/Performance/Performance.jsx
+++ src/components/Performance/Performance.jsx
@@ -7,13 +7,15 @@ const Performance = ({dataPerformance}) => {
         return item
     })
 
+    console.log(data);
+
     return (
         <div className='performance'>
         <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="subject" />
           <PolarRadiusAxis />
-          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+          <Radar name="Mike" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
         </RadarChart>
             
         </div>

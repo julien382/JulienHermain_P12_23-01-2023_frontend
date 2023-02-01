248a3dc7eac7aacd261c8be414311627440e73b8
julien382 <julhermain@gmail.com>
2023-02-01 15:31:18 +0100

    affichage valeur sessions

--- src/components/Sessions/Sessions.jsx
+++ src/components/Sessions/Sessions.jsx
@@ -8,27 +8,38 @@ const Sessions = ({dataSessions}) => {
         return { ...item, day: weekdays[item.day - 1] }
     })
 
+    console.log(data);
+
     return (
         <div className='sessions'>
             <h4>Durée moyenne des sessions</h4> 
             <LineChart
                 width={258}
                 height={263}
-                data={data.day}
+                data={data}
             >
-                <CartesianGrid  vertical={false} horizontal={false} />
-                <XAxis 
-                 dataKey="day"
-                 tick={{ fill: 'white', opacity: '.7' }}
-                 tickLine={false}
-                 tickMargin={5}
-                 interval="preserveStartEnd"
-                 axisLine={false}/>
-                <YAxis 
-                 hide={true} domain={['dataMin-20', 'dataMax+40']}/>
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey={data.sessionLength} stroke="#8884d8" activeDot={{ r: 8 }} />
+            <CartesianGrid  vertical={false} horizontal={false} />
+            <XAxis 
+                dataKey="day"
+                tick={{ fill: 'white', opacity: '.7' }}
+                tickLine={false}
+                tickMargin={5}
+                interval="preserveStartEnd"
+                axisLine={false}/>
+            <YAxis 
+                hide={true} domain={['dataMin-20', 'dataMax+40']}/>
+            <Tooltip
+                offset={21}
+                itemStyle={{
+                fontSize: 10,
+                }}
+                formatter={(value, unit) => [ unit, value]}
+                labelStyle={{ display: 'none' }}
+                contentStyle={{
+                }}
+            />
+            <Legend />
+            <Line type="monotone" name="min" dataKey="sessionLength" stroke="white" />
             </LineChart>
             
         </div>

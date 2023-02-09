c4b4e2c4ff6994ca5f2fdbba4d3e712d5364f99a
julien382 <julhermain@gmail.com>
2023-02-09 17:46:18 +0100

    add virgule kCal cardNutrition

--- src/components/CardNutrition/CardNutrition.jsx
+++ src/components/CardNutrition/CardNutrition.jsx
@@ -2,13 +2,22 @@ import './CardNutrition.css'
 
 const cardNutrition = ({containerLogo, logo, nutritionValue, nutritionType}) => {
 
+// split les chiffres et les lettres
+const [formattedNumber, unit] = nutritionValue.match(/^(\d+)(.*)$/).slice(1);
+
+// mettre une virgule
+const formattedValue = formattedNumber.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+
+// add unit
+const goodValueNutrition = formattedValue + unit;
+
     return (
         <div className='cardNutrition'>
             <div className={`logoCardNutrition ${containerLogo}`}>
                 <img src={logo} alt={containerLogo} className="logo"/>
             </div>
             <div className='texteCardNutrition'>
-                <h3>{nutritionValue}</h3>
+                <h3>{goodValueNutrition}</h3>
                 <p>{nutritionType}</p>
 
             </div>
